e36d2dd4d9467c98421958949fd99a389e18336a
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-07-28 03:23:36 +0600

    Added constant HttpMethods static class

---
+++ Symbiosis.Json.Specs/AspNetCore/Http/HttpMethods.cs
@@ -0,0 +1,10 @@
+﻿namespace Microsoft.AspNetCore.Http
+{
+	public static class HttpMethods
+	{
+		public const string GET = "GET";
+		public const string PATCH = "PATCH";
+		public const string POST = "POST";
+		public const string PUT = "PUT";
+	}
+}
