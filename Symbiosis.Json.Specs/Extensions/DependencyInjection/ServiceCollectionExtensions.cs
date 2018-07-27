6f3d46a828f7430c319731d93e8567265fab8b46
Rafsanul Hasan <rafsanulhasan@outlook.com>
2018-07-28 03:44:18 +0600

    Added PagingOptions Extension Method to IServiceCollection

---
+++ Symbiosis.Json.Specs/Extensions/DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+	public static class ServiceCollectionExtensions
+	{
+		public static IServiceCollection AddPagingOptions(this IServiceCollection services, IConfiguration configuration) 
+			=> services.Configure<PagingOptions>(configuration);
+	}
+}
